870a707fd1e66d420f58781130b90a646fffbedc
Luca Berbenni <lucaberbenni@mbp-di-luca.localdomain>
2023-07-04 17:57:13 +0200

    created dataset

--- text_generator.py
+++ text_generator.py
@@ -24,5 +24,31 @@ chars_from_ids = tf.keras.layers.StringLookup(
 )
 chars = chars_from_ids(ids)
 
-text_from_chars = tf.strings.reduce_join(chars, axis=-1).numpy()
+def text_from_ids(ids):
+    return tf.strings.reduce_join(chars_from_ids(ids), axis=-1)
+
+all_ids = ids_from_chars(tf.strings.unicode_split(text, 'UTF-8'))
+
+ids_dataset = tf.data.Dataset.from_tensor_slices(all_ids)
+
+seq_length = 100
+examples_per_epoch = len(text) // (seq_length + 1)
+
+sequences = ids_dataset.batch(seq_length + 1, drop_remainder = True)
+
+def split_input_target(sequence):
+    input_text = sequence[:-1]
+    target_text = sequence[1:]
+    return input_text, target_text
+
+dataset = sequences.map(split_input_target)
+
+BATCH_SIZE = 64
+BUFFER_SIZE = 10000
+
+dataset = (
+    dataset.shuffle(BUFFER_SIZE)
+    .batch(BATCH_SIZE, drop_remainder=True)
+    .prefetch(tf.data.experimental.AUTOTUNE)
+)
 
