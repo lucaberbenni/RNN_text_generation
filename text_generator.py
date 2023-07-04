a75a409e273bed679fbb11ec500ae4f9aff2460c
Luca Berbenni <lucaberbenni@mbp-di-luca.localdomain>
2023-07-04 20:14:09 +0200

    defined model architecture

--- text_generator.py
+++ text_generator.py
@@ -52,3 +52,32 @@ dataset = (
     .prefetch(tf.data.experimental.AUTOTUNE)
 )
 
+vocab_size = len(vocab)
+embedding_dim = 256
+rnn_units = 1024
+
+class MyModel(tf.keras.Model):
+    def __init__(self, vocab_size, embedding_dim, rnn_units):
+        super().__init__(self)
+        self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+        self.gru = tf.keras.layers.GRU(rnn_units, return_state=True, return_state=True)
+        self.dense = tf.keras.layers.Dense(vocab_size)
+    def call(self, inputs, states=None, return_state=False, training=False):
+        x = self.embedding(inputs, training=training)
+        if states is None:
+            states = self.gru.get_initial_state(x)
+
+        x, states = self.gru(x, initial_state=states, training=training)
+        x = self.dense(x, training=training)
+
+        if return_state:
+            return x, states
+        else:
+            return x
+
+model = MyModel(
+    vocab_size=len(ids_from_chars.get_vocabulary()), 
+    embedding_dim=embedding_dim, 
+    rnn_units=rnn_units
+)
+
